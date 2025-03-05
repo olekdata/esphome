0c2575b745e31f08cb92ebb2500ac355354ba268
Aleksander Data <54954537+olekdata@users.noreply.github.com>
2025-03-05 23:08:59 +0100

    a

---
+++ components/my_component/sensor.py
@@ -0,0 +1,19 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import UNIT_CELSIUS, DEVICE_CLASS_TEMPERATURE
+
+DEPENDENCIES = []
+
+my_sensor_ns = cg.esphome_ns.namespace('my_sensor')
+MySensor = my_sensor_ns.class_('MySensor', sensor.Sensor)
+
+CONFIG_SCHEMA = sensor.sensor_schema(
+    MySensor,
+    unit_of_measurement=UNIT_CELSIUS,
+    device_class=DEVICE_CLASS_TEMPERATURE
+).extend({})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config)
+    await sensor.register_sensor(var, config)
