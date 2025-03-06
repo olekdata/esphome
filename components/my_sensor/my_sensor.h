23326de93b3e88ee07e3f5d170beff6bed044338
Aleksander Data <54954537+olekdata@users.noreply.github.com>
2025-03-06 08:28:40 +0100

    Update my_sensor.h

--- components/my_sensor/my_sensor.h
+++ components/my_sensor/my_sensor.h
@@ -2,9 +2,11 @@
 
 #include "esphome.h"
 
+!!! ma być błąd
+
 class MySensor : public PollingComponent, public sensor::Sensor {
 public:
-    MySensor() : !!PollingComponent(5000) {}  // Odczyt co 5 sekund
+    MySensor() : PollingComponent(5000) {}  // Odczyt co 5 sekund
 
     void setup() override {
         ESP_LOGD("my_sensor", "Czujnik niestandardowy - inicjalizacja");
