5d8b27a4d84cc6b67247ae3f13fa55ae0754da1d
Aleksander Data <54954537+olekdata@users.noreply.github.com>
2025-03-06 19:10:43 +0100

    Update my_sensor.h

--- components/my_sensor/my_sensor.h
+++ components/my_sensor/my_sensor.h
@@ -2,18 +2,16 @@
 
 #include "esphome.h"
 
-//!!! ma być błąd
-
 class MySensor : public PollingComponent, public sensor::Sensor {
 public:
     MySensor() : PollingComponent(5000) {}  // Odczyt co 5 sekund
 
     void setup() override {
-        ESP_LOGD("my_sensor", "Czujnik niestandardowy - inicjalizacja");
+        ESP_LOGD("my_sensor", "Inicjalizacja czujnika");
     }
 
     void update() override {
-        float temperature = random(2000, 3000) / 100.0;  // Losowa temperatura 20-30°C
+        float temperature = random(2000, 3000) / 100.0;  // Losowa temp. 20-30°C
         ESP_LOGD("my_sensor", "Nowa temperatura: %.2f°C", temperature);
         publish_state(temperature);
     }
