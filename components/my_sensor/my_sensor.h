38b64ab86f7592e1d49aa2bee85b7ee9dc951dec
Aleksander Data <54954537+olekdata@users.noreply.github.com>
2025-03-06 20:49:18 +0100

    Update my_sensor.h

--- components/my_sensor/my_sensor.h
+++ components/my_sensor/my_sensor.h
@@ -9,7 +9,7 @@ namespace my_sensor {
 class MySensor : public PollingComponent, public sensor::Sensor {
 public:
     // Konstruktor z czasem odczytu w milisekundach
-    //MySensor(uint32_t update_interval = 5000);
+    MySensor(uint32_t update_interval = 5000);
 
     void setup() override;   // Inicjalizacja komponentu
     void update() override;  // Odczyt danych i aktualizacja wartości
